be0bfc385a6f5bbc2839e2b243c80dbc5dca9dc0
Rukki0109 <lukiya0109@icloud.com>
2025-03-05 02:14:37 +0900

    profile follow

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -3,6 +3,7 @@ import Topbar from '../../compornents/topbar/Topbar';
 import Sidebar from '../../compornents/sidebar/Sidebar';
 import Timeline from '../../compornents/timeline/Timeline';
 import Rightbar from '../../compornents/rightbar/Rightbar';
+import Follow from '../../compornents/follow/Follow';
 import "./Profile.css"
 import axios from "axios";
 import { useParams } from 'react-router-dom';
@@ -13,13 +14,13 @@ export default function Profile() {
     const username = useParams().username;
 
     useEffect(() => {
-      const fetchUser = async()=>{
-        const response = await axios.get(`${process.env.REACT_APP_API_URL}/users?username=${username}`);
-        // console.log(response);
-        setUser(response.data);
-      }
-      fetchUser();
-    },[]);
+        const fetchUser = async () => {
+            const response = await axios.get(`${process.env.REACT_APP_API_URL}/users?username=${username}`);
+            // console.log(response);
+            setUser(response.data);
+        }
+        fetchUser();
+    }, []);
 
     return (
         <>
@@ -29,21 +30,23 @@ export default function Profile() {
                 <div className="profileRight">
                     <div className="profileRightTop">
                         <div className="profileCover">
-                            <img src={user.coverPicture || PUBLIC_FOLDER+"/post/3.jpeg"} alt="" className="profileCoverImg" />
+                            <img src={user.coverPicture || PUBLIC_FOLDER + "/post/3.jpeg"} alt="" className="profileCoverImg" />
                             <img src={
-                            user.profilePicture
-                            ?PUBLIC_FOLDER + user.profilePicture
-                            :PUBLIC_FOLDER + "/person/noAvatar.png"}
-                             alt="" className="profileUserImg" />
+                                user.profilePicture
+                                    ? PUBLIC_FOLDER + user.profilePicture
+                                    : PUBLIC_FOLDER + "/person/noAvatar.png"}
+                                alt="" className="profileUserImg" />
                         </div>
                     </div>
                     <div className="profileInfo">
                         <h4 className='profileInfoName'>{user.username}</h4>
                         <span className="profileInfoDesc">{user.desc}</span>
+                        {/* 自身のプロフィールページではない場合のみ <Follow /> を表示 */}
+                        {(!username || username !== user.username) && <Follow />}
                     </div>
                     <div className="profileRightBottom">
-                        <Timeline username={username}/>
-                        <Rightbar user ={user}/>
+                        <Timeline username={username} />
+                        <Rightbar user={user} />
                     </div>
                 </div>
             </div>
