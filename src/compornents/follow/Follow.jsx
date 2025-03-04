971f10464977ac3a8a61fcfd2c187c9b198aba35
Rukki0109 <lukiya0109@icloud.com>
2025-03-05 02:18:08 +0900

    profile follow

--- src/compornents/follow/Follow.jsx
+++ src/compornents/follow/Follow.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 export default function Follow() {
-  return (
-    <div>Follow</div>
-  )
+    return (
+        <>
+            <button onClick={handleFollow} className={`follow-button ${isFollowing ? "unfollow" : "follow"}`}>
+                {isFollowing ? "フォロー解除" : "フォロー"}
+            </button>
+        </>
+    )
 }
